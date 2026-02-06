0809972ee8d05eb0c764253d3436f83e84e0b098
Vyshnav K B <vyshnavkb648@gmail.com>
2026-02-07 00:34:31 +0530

    7: Passed dynamic data from views to templates

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'pages/home.html')
+    context = {
+        "name" : "Vy",
+        "role" : "Python Developer",
+        "day" : 7,
+        "status" : "Learning Django consistently 💪"
+    }
+    return render(request, 'pages/home.html' , context)
+
 
 def about(request):
     return render(request, 'pages/about.html')
