21cdc2e1c7df54367c870e4d2e9422ee1cb24369
Vyshnav K B <vyshnavkb648@gmail.com>
2026-02-27 00:25:58 +0530

    9: Added Profile model, migrations, and admin integration

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
