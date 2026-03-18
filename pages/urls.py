6bd5e7281ef0aa610aad5ecd8260cc1672c9e08b
Vyshnav K B <vyshnavkb648@gmail.com>
2026-03-18 23:28:12 +0530

    11: Added Django ModelForm to create Profile from website

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('add-profile/', views.add_profile, name='add_profile'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Profile
+from .forms import ProfileForm
+from django.shortcuts import redirect
+
 
 def home(request):
     profiles = Profile.objects.all()   # fetch all rows
@@ -11,3 +14,18 @@ def home(request):
 
 def about(request):
     return render(request, 'pages/about.html')
+
+#Add profile form function :
+def add_profile(request):
+
+    if request.method == "POST":
+        form = ProfileForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    else:
+        form = ProfileForm()
+
+    return render(request, 'pages/add_profile.html', {'form': form})
